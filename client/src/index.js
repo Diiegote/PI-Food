e1f2b9fbb25969951a2c9d2cf106b0fe65fab3d9
unknown <diego.elprado22@gmail.com>
2022-08-20 19:07:01 -0300

    asdasd

--- client/src/index.js
+++ client/src/index.js
@@ -6,10 +6,11 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import {Provider} from "react-redux";
 import store from "./Redux/Store/store"
-// import dotenv from "dotenv";
-// import axios from "axios"
-// dotenv.config();
-// axios.defaults.baseURL=process.env.REACT_APP_API||"http://localhost:3001";
+import axios from "axios"
+import dotenv from "dotenv";
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
   <Provider store={store}>
