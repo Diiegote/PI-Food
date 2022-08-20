f013b4d50e31ad713fafdd3cb5b6fdf18155ce3e
unknown <diego.elprado22@gmail.com>
2022-08-20 12:42:11 -0300

    fixed bug

--- api/src/db.js
+++ api/src/db.js
@@ -30,7 +30,7 @@ let sequelize =
         ssl: true,
       })
     : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/development`,
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`,
         { logging: false, native: false }
       );
 
